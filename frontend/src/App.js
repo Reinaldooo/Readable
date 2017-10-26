2abda09e66663b8d690badf7112203be65261f0b
Reinaldo Trindade <rewifetri@gmail.com>
2017-10-26 15:44:22 -0200

    Comment page design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import './App.css'
 import moment from 'moment'
-// import sortBy from 'sort-by'
 import {connect} from 'react-redux'
 import {getPosts, getCategories, ratePost, deletePost, getPostsCategorized} from './actions'
 //import uuidv4 from 'uuid/v4'
@@ -94,7 +93,80 @@ render() {
                   }
                 </div>
             </div>
-        }                    
+        }
+
+
+
+
+        <div className="container">
+            <div className="post-detail-page">
+            {this.props.posts[5] &&
+              <a key={this.props.posts[5].id} className="list-group-item list-group-item-action flex-column align-items-start">
+                <div className="d-flex w-100 justify-content-between">
+                  <h5 className="mb-1"><i className="fa fa-angle-right" aria-hidden="true"></i> {this.props.posts[5].title}</h5>
+                  {this.props.posts[5].edited ? <small>{moment.utc(this.props.posts[5].timestamp).format("ddd, MMM Do YYYY, h:mm a")}<strong><span className="orange-focus"> - Edited</span></strong></small>
+                  :
+                  <small>{moment.utc(this.props.posts[5].timestamp).format("ddd, MMMM Do YYYY, h:mm a")}</small>}
+                </div>
+                {
+                <p className="mb-1-body">{this.props.posts[5].body}</p>} 
+                <small className="post-details">Author: <strong>{this.props.posts[5].author}</strong> • <strong className="score">{this.props.posts[5].voteScore} {this.props.posts[5].voteScore === 1 || this.props.posts[5].voteScore === -1 ? <span className="post-count">point</span> : <span className="post-count">points</span>}</strong> • <strong><span className="orange-focus"><i className="fa fa-tag" aria-hidden="true"></i> {this.props.posts[5].category}</span></strong> • {this.props.posts[5].comments.length} {this.props.posts[5].comments.length === 1 ? "comment" : "comments"}</small>
+                <div className="btn-group" role="group" aria-label="up and downvote">
+                  <button type="button" className="button"><i className="fa fa-thumbs-up" aria-hidden="true"></i></button>
+                  <button type="button" className="button"><i className="fa fa-thumbs-down" aria-hidden="true"></i></button>
+                </div>                
+                <div className="btn-group btn-custom" role="group" aria-label="Edit and Delete">
+                  <button type="button" className="button"><i className="fa fa-pencil-square-o" aria-hidden="true"></i></button>
+                  <button onClick={() => this.props.deletePost(this.props.posts[5].id)}type="button" className="button delete"><i className="fa fa-trash-o" aria-hidden="true"></i></button>
+                </div>
+              </a>
+          }
+            </div>
+            <div>
+            {this.props.posts[5] &&
+              this.props.posts[5].comments ? 
+              <a key={this.props.posts[5].comments[0].id} className="list-group-item list-group-item-action flex-column align-items-start comments">
+                <div className="d-flex w-100 justify-content-between">
+                  <small className="post-details">Comment by: <strong>{this.props.posts[5].comments[0].author}</strong></small>
+                  <small>{moment.utc(this.props.posts[5].comments[0].timestamp).format("ddd, MMMM Do YYYY, h:mm a")}</small>
+                </div>
+                <p className="mb-1-body"><i className="fa fa-angle-right" aria-hidden="true"></i> {this.props.posts[5].comments[0].body}</p>
+                <small className="post-details"><strong className="score">{this.props.posts[5].comments[0].voteScore} {this.props.posts[5].comments[0].voteScore === 1 || this.props.posts[5].comments[0].voteScore === -1 ? <span className="post-count">point</span> : <span className="post-count">points</span>}</strong></small>
+                <div className="btn-group" role="group" aria-label="up and downvote">
+                  <button type="button" className="button"><i className="fa fa-thumbs-up" aria-hidden="true"></i></button>
+                  <button type="button" className="button"><i className="fa fa-thumbs-down" aria-hidden="true"></i></button>
+                </div>                
+                <div className="btn-group btn-custom" role="group" aria-label="Edit and Delete">
+                  <button type="button" className="button"><i className="fa fa-pencil-square-o" aria-hidden="true"></i></button>
+                  <button onClick={() => this.props.deletePost(this.props.posts[5].comments[0].id)}type="button" className="button delete"><i className="fa fa-trash-o" aria-hidden="true"></i></button>
+                </div>
+              </a> :
+              <div></div>
+          }
+            </div>
+            <div>
+            {this.props.posts[5] &&
+              this.props.posts[5].comments ? 
+              <a key={this.props.posts[5].comments[1].id} className="list-group-item list-group-item-action flex-column align-items-start comments">
+                <div className="d-flex w-100 justify-content-between">
+                  <small className="post-details">Comment by: <strong>{this.props.posts[5].comments[0].author}</strong></small>
+                  <small>{moment.utc(this.props.posts[5].comments[0].timestamp).format("ddd, MMMM Do YYYY, h:mm a")}</small>
+                </div>
+                <p className="mb-1-body"><i className="fa fa-angle-right" aria-hidden="true"></i> {this.props.posts[5].comments[1].body}</p>
+                <small className="post-details"><strong className="score">{this.props.posts[5].comments[0].voteScore} {this.props.posts[5].comments[0].voteScore === 1 || this.props.posts[5].comments[0].voteScore === -1 ? <span className="post-count">point</span> : <span className="post-count">points</span>}</strong></small>
+                <div className="btn-group" role="group" aria-label="up and downvote">
+                  <button type="button" className="button"><i className="fa fa-thumbs-up" aria-hidden="true"></i></button>
+                  <button type="button" className="button"><i className="fa fa-thumbs-down" aria-hidden="true"></i></button>
+                </div>                
+                <div className="btn-group btn-custom" role="group" aria-label="Edit and Delete">
+                  <button type="button" className="button"><i className="fa fa-pencil-square-o" aria-hidden="true"></i></button>
+                  <button onClick={() => this.props.deletePost(this.props.posts[5].comments[0].id)}type="button" className="button delete"><i className="fa fa-trash-o" aria-hidden="true"></i></button>
+                </div>
+              </a> :
+              <div></div>
+          }
+            </div>
+        </div>                    
         </div>
     );
   }
@@ -102,23 +174,23 @@ render() {
 
 const mapStateToProps = (state) => {
   return {
-      posts: state.posts,
-      categories: state.categories,
-      postsHasErrored: state.postsHasErrored,
-      postsAreLoading: state.postsAreLoading,
+      posts:                state.posts,
+      categories:           state.categories,
+      postsHasErrored:      state.postsHasErrored,
+      postsAreLoading:      state.postsAreLoading,
       categoriesHasErrored: state.categoriesHasErrored,
       categoriesAreLoading: state.categoriesAreLoading,
-      postsCount: state.postsCount
+      postsCount:           state.postsCount
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-      getPosts: () => dispatch(getPosts()),
+      getPosts: ()                    => dispatch(getPosts()),
       getPostsCategorized: (category) => dispatch(getPostsCategorized(category)),
-      getCategories: () => dispatch(getCategories()),
-      ratePost: (rate, id, index) => dispatch(ratePost(rate, id, index)),
-      deletePost: (id) => dispatch(deletePost(id))
+      getCategories: ()               => dispatch(getCategories()),
+      ratePost: (rate, id, index)     => dispatch(ratePost(rate, id, index)),
+      deletePost: (id)                => dispatch(deletePost(id))
   };
 };
 
--- server/posts.js
+++ server/posts.js
@@ -29,7 +29,7 @@ const defaultData = {
     id: '3',
     timestamp: 1507907893799,
     title: 'Should the reviewer approve my project?',
-    body: "I hope so! I'll add some non-sense text here just so you can see that if a post is too long, it will show up cropped.",
+    body: "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.",
     author: 'Student',
     category: 'udacity',
     voteScore: 12,
