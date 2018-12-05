47f1f49f53b31ef4f04386fce381ded4b3f47528
Reinaldo Trindade <rewifetri@gmail.com>
2018-12-05 21:57:06 -0200

    heroku-postbuild script update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,10 +7,9 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 import { Provider } from 'react-redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
 
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
 registerServiceWorker()
